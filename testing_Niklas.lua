b7f19f2b8ebc97986522326d9b221cd585a9b99b
Niklas <coding@niklasdeines.de>
2026-04-22 16:06:02 +0200

    edit item names

--- testing_Niklas.lua
+++ testing_Niklas.lua
@@ -2,10 +2,19 @@ local me = peripheral.find("me_bridge")
 
 if not me then 
     print("Keine Bridge gefunden")
+    return
 end
 
 local items = me.getItems()
 
 for _, item in ipairs(items) do
-    print(item.name)
+    local name = item.name
+
+    -- Mod-Präfix entfernen
+    name = string.match(name, ":(.+)") or name
+
+    -- Unterstriche durch Leerzeichen ersetzen
+    name = string.gsub(name, "_", " ")
+
+    print(name)
 end
